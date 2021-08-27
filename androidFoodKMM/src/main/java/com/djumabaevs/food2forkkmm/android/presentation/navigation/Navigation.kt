d66809b8e30e0cee8e5d2c1d63a96f9f4a27f7f4
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-28 00:09:40 +0600

    improved navigation

--- androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/navigation/Navigation.kt
+++ androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/navigation/Navigation.kt
@@ -5,23 +5,30 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.djumabaevs.food2forkkmm.android.presentation.recipe_detail.RecipeDetailScreen
+import com.djumabaevs.food2forkkmm.android.presentation.recipe_list.RecipeListScreen
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.RecipeList.route) {
         composable(route = Screen.RecipeList.route) {navBackStackEntry ->
-            Text("Recipe list screen")
-            Divider()
-            Button(onClick = { navController.navigate(Screen.RecipeDetail.route) }) {
-                Text("Go recipe detail")
-            }
+           RecipeListScreen(onSelectedRecipe = { recipeId ->
+               navController.navigate(Screen.RecipeDetail.route + "/$recipeId")
+           })
         }
-        composable(route = Screen.RecipeDetail.route) {navBackStackEntry ->
-            Text(text = "Recipe detail screen")
+        composable(
+            route = Screen.RecipeDetail.route + "/{recipeId}",
+            arguments = listOf(navArgument("recipeId") {
+                type = NavType.IntType
+            })
+        ) {navBackStackEntry ->
+            RecipeDetailScreen(recipeId = navBackStackEntry.arguments?.getInt("recipeId"))
         }
     }
 }
--- androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
+++ androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -1,10 +1,15 @@
 package com.djumabaevs.food2forkkmm.android.presentation.recipe_detail
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun RecipeDetailScreen(
-    recipeId: Int
+    recipeId: Int?
 ) {
-
+    if(recipeId == null) {
+        Text("Error")
+    } else {
+        Text(text = "Recipe id detail screen: $recipeId")
+    }
 }
