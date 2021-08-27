d65f490397049ac1397e928e40ec7073ac7d5187
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-28 00:22:53 +0600

    second view model for every screen

---
+++ androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_detail/RecipeDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.djumabaevs.food2forkkmm.android.presentation.recipe_detail
+
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeDetailViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+){
+}
--- androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_list/RecipeListViewModel.kt
+++ androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_list/RecipeListViewModel.kt
@@ -1,8 +1,11 @@
 package com.djumabaevs.food2forkkmm.android.presentation.recipe_list
 
+import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class RecipeListViewModel @Inject constructor() {
+class RecipeListViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle //don't need for this VM
+) {
 }
