68756e643fa4448561032ec854c77b241d301119
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-28 00:32:23 +0600

    savedStateHandle in init of viewModel

--- androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_detail/RecipeDetailViewModel.kt
+++ androidFoodKMM/src/main/java/com/djumabaevs/food2forkkmm/android/presentation/recipe_detail/RecipeDetailViewModel.kt
@@ -1,5 +1,7 @@
 package com.djumabaevs.food2forkkmm.android.presentation.recipe_detail
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,4 +10,16 @@ import javax.inject.Inject
 class RecipeDetailViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ){
+    private val recipeId: MutableState<Int?> = mutableStateOf(null)
+
+    init {
+        try {
+            savedStateHandle.get<Int>("recipeId")?.let { recipeId ->
+                this.recipeId.value = recipeId
+            }
+        } catch (e: Exception) {
+         //will throw an exception if an arg is not there for whatever reason
+        }
+    }
+
 }
